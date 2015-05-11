2125ef4e955b73b48400fe1b269ffcfa39659173
BenniMagg <bennim99@gmail.com>
2015-05-11 15:16:59 +0200

    This is the GUI with corrected buttons and layout (Mother fucckers!)

---
+++ ETS2_AGA-master/app/src/main/java/com/example/eyjobro/ets2_aga/main_screen.java
@@ -0,0 +1,46 @@
+package com.example.eyjobro.ets2_aga;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class main_screen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return true;
+    }
+
+    public void openCreateProfile(View view){
+        Intent intent = new Intent(this, create_profile.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+}
